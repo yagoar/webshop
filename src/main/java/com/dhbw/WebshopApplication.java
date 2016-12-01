e4c30370526f0c3e141219a08ec27e0b321dfc45
Yaiza Gonzalo Alt <yaiza.alt@gmail.com>
2016-12-01 13:06:03 +0100

    Initial commit

---
+++ src/main/java/com/dhbw/WebshopApplication.java
@@ -0,0 +1,12 @@
+package com.dhbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopApplication.class, args);
+	}
+}
