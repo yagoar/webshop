9f71fa8ad6aca3061399be04094891946e870883
thisisstone <jensgerle@gmail.com>
2017-03-19 17:02:45 +0100

    first draft: fileUpload

--- src/main/java/com/dhbw/config/JerseyConfig.java
+++ src/main/java/com/dhbw/config/JerseyConfig.java
@@ -1,5 +1,6 @@
 package com.dhbw.config;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,9 @@ public class JerseyConfig extends ResourceConfig
 {
 
   public JerseyConfig() {
+
     packages("com.dhbw.api");
+    register(MultiPartFeature.class);
   }
 
 }
