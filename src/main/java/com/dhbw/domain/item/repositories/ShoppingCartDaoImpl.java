d09cff3422f34cae1909a923d62ab35707280a85
thisisstone <jensgerle@gmail.com>
2017-03-27 12:57:01 +0200

    update stock of items when placing order

--- src/main/java/com/dhbw/domain/item/repositories/ShoppingCartDaoImpl.java
+++ src/main/java/com/dhbw/domain/item/repositories/ShoppingCartDaoImpl.java
@@ -1,11 +1,14 @@
 package com.dhbw.domain.item.repositories;
 
+import com.dhbw.domain.item.BaseItem;
+import com.dhbw.domain.item.ItemAndQuantity;
 import com.dhbw.domain.item.ShoppingCart;
 import com.dhbw.domain.item.ShoppingOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.Set;
 
 /**
  * Created by jgerle on 26.02.2017.
@@ -15,10 +18,18 @@ public class ShoppingCartDaoImpl implements ShoppingCartDaoCustom {
 
     @Autowired
     private ShoppingOrderDao shoppingOrderDao;
+    @Autowired
+    private BaseItemDao baseItemDao;
 
     @Override
     public void placeOrder(ShoppingCart cart) {
         ShoppingOrder order = new ShoppingOrder(cart.getUser(), new Date(), cart.getItems());
+        Set<ItemAndQuantity> items = cart.getItems();
+        for(ItemAndQuantity item : items) {
+            BaseItem it = item.getItem();
+            it.setStock(it.getStock() - item.getQuantity());
+            baseItemDao.save(it);
+        }
         cart.setShoppingOrder(order);
         shoppingOrderDao.save(order);
         cart.getItems().clear();
