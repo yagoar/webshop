9c77c1063be63eab3a48e0e666aaf01208effc3f
Yaiza Gonzalo Alt <yaiza.alt@gmail.com>
2017-04-15 15:29:05 +0200

    Jersey register Endpoints individually

--- src/main/java/com/dhbw/config/JerseyConfig.java
+++ src/main/java/com/dhbw/config/JerseyConfig.java
@@ -1,5 +1,12 @@
 package com.dhbw.config;
 
+import com.dhbw.api.admin.AdminEndpointImpl;
+import com.dhbw.api.authentication.AuthenticationEndpoint;
+import com.dhbw.api.fileUpload.UploadFileService;
+import com.dhbw.api.items.ItemsEndpointImpl;
+import com.dhbw.api.shoppingCart.ShoppingCartEndpointImpl;
+import com.dhbw.api.shoppingOrder.ShoppingOrderEndpointImpl;
+import com.dhbw.api.user.UserEndpointImpl;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +18,15 @@ import javax.ws.rs.ApplicationPath;
 public class JerseyConfig extends ResourceConfig
 {
   public JerseyConfig() {
-    packages("com.dhbw.api");
+    //Register all REST Endpoints - always use implementation, not interface
+    register(AdminEndpointImpl.class);
+    register(AuthenticationEndpoint.class);
+    register(ItemsEndpointImpl.class);
+    register(UploadFileService.class);
+    register(ShoppingCartEndpointImpl.class);
+    register(ShoppingOrderEndpointImpl.class);
+    register(UserEndpointImpl.class);
+
     register(MultiPartFeature.class);
   }
 }
